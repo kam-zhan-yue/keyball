db36a99fd32839b2f09404ebcd69768b5ddee7f4
Alex Kam <kamzhanyue@gmail.com>
2025-12-14 12:14:28 +0900

    second edition

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/kam-zhan-yue/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/kam-zhan-yue/keymap.c
@@ -27,29 +27,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
     KC_ESC   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_ENT   ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-              KC_LOPT,KC_LGUI,   MO(1),   KC_LCTL    ,MO(3),                  KC_BSPC,KC_SPC, _______, _______  , KC_PSCR
+              KC_LOPT,KC_LGUI,   MO(1),   MO(2) ,  LT(3,KC_LCTL),                  KC_BSPC,KC_SPC, _______, _______  , KC_PSCR
   ),
   // Layer 1 - Symbol Layer
   [1] = LAYOUT_universal(
-    _______ ,  _______ , S(KC_7)  , S(KC_8)   , S(KC_9)    , _______ ,                         _______ , KC_MINUS , S(KC_MINUS), KC_EQUAL    , S(KC_EQUAL)   , _______   ,
-    _______ ,  LM(2, MOD_LALT | MOD_LSFT) , S(KC_4)  , S(KC_5)   , S(KC_6)    , _______ ,      KC_QUOTE    , S(KC_0)  , S(KC_9)    , S(KC_RBRC)  , S(KC_LBRC)  , _______   ,
-    MO(2) ,  LM(2, MOD_LALT) , S(KC_1)  , S(KC_2)   , S(KC_3)    , _______ ,                    S(KC_QUOTE)     , KC_RBRC  , KC_LBRC    , KC_BSLS  , S(KC_BSLS)  , _______  ,
-                  _______  , LM(2, MOD_LGUI) , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+    _______ ,  KC_GRAVE    , S(KC_5)  , S(KC_6)    , S(KC_7) , S(KC_8) ,                   _______ , S(KC_EQUAL) , KC_BSLS, S(KC_BSLS)   , _______   , _______   ,
+    _______ ,  S(KC_COMM) , KC_EQUAL , KC_MINUS, S(KC_MINUS), S(KC_DOT) ,                 KC_QUOTE    , S(KC_0)  , S(KC_9)    , S(KC_RBRC)  , S(KC_LBRC)  , _______   ,
+    _______ ,  S(KC_GRAVE)    , S(KC_1)  , S(KC_2)    , S(KC_3) , S(KC_4),                    S(KC_QUOTE)     , KC_RBRC  , KC_LBRC    , _______  , _______  , _______  ,
+                  _______  , LM(2, MOD_LGUI) , _______  ,                     _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
   ),
   // Layer 2 - Num Layer
   [2] = LAYOUT_universal(
-    _______  ,  _______   , _______    , _______   , _______    , _______    ,                             _______    , KC_7    , KC_8    , KC_9    , KC_MINUS   , _______   ,
-    _______  ,  _______ , _______  , _______   , _______   , _______   ,                                     _______  , KC_4    , KC_5    , KC_6    , S(KC_EQUAL)  , KC_SLSH  ,
-    _______  ,  KC_LSFT , KC_LCTL , KC_LOPT  , KC_LGUI  , _______  ,                                         KC_0     , KC_1    , KC_2    , KC_3    , S(KC_8)  , KC_EQUAL  ,
-                  _______  , _______ , _______  ,   _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+    _______  ,  KC_F1, KC_F2 , KC_F3  , KC_F4 , KC_F5 ,                                      KC_F6 , KC_F7   , KC_F8    , KC_F9   , KC_F10   , KC_F11   ,
+    KC_LOPT  ,  KC_1 , KC_2  , KC_3   , KC_4  , KC_5  ,                                     KC_6  , KC_7    , KC_8    , KC_9    , KC_0  , KC_F12 ,
+    KC_LGUI  ,  _______ , _______ , _______  , _______  , _______  ,                        _______     , _______    , _______    , _______    , _______  , _______  ,
+                  _______  , _______ , _______  ,   _______  , _______  ,           _______  , _______  , _______       , _______  , _______
   ),
   // TODO: Figure this out lol
   // Layer 3 - Unused / Modifiers
   [3] = LAYOUT_universal(
     _______  ,  _______   , _______    , _______   , _______    , _______    ,                             _______    , _______    , _______    , _______    , _______   , _______   ,
     _______  ,  _______ , _______  , _______   , _______   , _______   ,                                     KC_LEFT  , KC_DOWN    , KC_UP    , KC_RIGHT  , _______  , _______  ,
-    _______  ,  _______ , _______  , _______ , _______  , _______  ,                                         _______  , KC_LNG1  , KC_LNG2    , _______  , _______  , _______  ,
-                  QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
+    _______  ,  _______ , _______  , _______ , _______  , _______  ,                                         _______  , _______  , _______    , _______  , _______  , _______  ,
+                  _______  , _______  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
   ),
   // TODO: Add more buttons
   // Mouse Layer
