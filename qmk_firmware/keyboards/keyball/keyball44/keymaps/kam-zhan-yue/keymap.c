2ae854ba29f37d254876827259f69e2c71558888
Alex Kam <kamzhanyue@gmail.com>
2026-01-03 12:30:54 +0900

    change num layer to support comfortable modifiers

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/kam-zhan-yue/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/kam-zhan-yue/keymap.c
@@ -39,8 +39,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Layer 2 - Num Layer
   [2] = LAYOUT_universal(
     _______  ,  KC_F1, KC_F2 , KC_F3  , KC_F4 , KC_F5 ,                                      KC_F6 , KC_F7   , KC_F8    , KC_F9   , KC_F10   , KC_F11   ,
-    KC_LOPT  ,  KC_1 , KC_2  , KC_3   , KC_4  , KC_5  ,                                     KC_6  , KC_7    , KC_8    , KC_9    , KC_0  , KC_F12 ,
-    KC_LGUI  ,  LM(2, MOD_LSFT | MOD_LALT) , _______ , _______  , _______  , _______  ,                  _______     , _______    , _______    , _______    , _______  , _______  ,
+    _______  ,  KC_1 , KC_2  , KC_3   , KC_4  , KC_5  ,                                     KC_6  , KC_7    , KC_8    , KC_9    , KC_0  , KC_F12 ,
+    _______  ,  _______ , _______ , _______  , _______  , _______  ,                  _______     , KC_RGUI    , KC_RALT    , KC_RSFT , KC_RCTL , KC_R  ,
                   _______  , _______ , _______  ,   _______  , _______  ,           _______  , _______  , _______       , _______  , _______
   ),
   // TODO: Figure this out lol
